0dc538a527951ba28522c64ec139f40edebcf159
João Palmeiro <joaommpalmeiro@gmail.com>
2020-03-16 23:55:47 +0000

    Added: Day 1: Quartiles.

---
+++ 10-Days-of-Statistics/Python/day-1_quartiles.py
@@ -0,0 +1,25 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+
+X = list(map(int, input().split()))
+
+X.sort()
+
+
+def median(n, X):
+    if(n % 2 == 0):
+        numerator = X[int(n/2)] + X[int(n/2 - 1)]
+        median_value = numerator / 2
+    else:
+        median_value = X[int(n/2)]
+
+    return(int(median_value))
+
+
+print(median(int(n/2), X[:int(n/2)]))
+print(median(n, X))
+if(n % 2 == 0):
+    print(median(int(n/2), X[int(n/2):]))
+else:
+    print(median(int(n/2), X[int(n/2)+1:]))
