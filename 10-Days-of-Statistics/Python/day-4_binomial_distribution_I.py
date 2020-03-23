e73eedcb0586575eec981653d1b8fc0d72817384
João Palmeiro <joaommpalmeiro@gmail.com>
2020-03-23 12:55:50 +0000

    Added: Day 4: Binomial Distribution I.

---
+++ 10-Days-of-Statistics/Python/day-4_binomial_distribution_I.py
@@ -0,0 +1,32 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from math import factorial
+
+i = "1.09 1"
+
+i = list(map(float, i.split()))
+
+p = i[0] / sum(i)
+# p = 0.5
+
+x = 3
+# x = 5
+
+n = 6
+# n = 10
+
+
+def binomial_pmf(x, n, p):
+    f = factorial(n) / (factorial(x) * factorial(n - x))
+    b = f * p ** x * (1-p) ** (n-x)
+
+    return(b)
+
+
+def at_least_cdf(x, n, p):
+    b = [binomial_pmf(x_r, n, p) for x_r in range(x, n+1)]
+
+    return(round(sum(b), 3))
+
+
+print(at_least_cdf(x, n, p))
