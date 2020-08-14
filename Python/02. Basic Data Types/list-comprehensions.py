05e8ac9d6b573d6150791c747d6fa6ca5ece1ac6
João Palmeiro <joaommpalmeiro@gmail.com>
2020-08-14 21:52:51 +0100

    Updated: Subfolders.

---
+++ Python/02. Basic Data Types/list-comprehensions.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    x = int(input()) + 1
+    y = int(input()) + 1
+    z = int(input()) + 1
+    n = int(input())
+
+    coordinates = [
+        [i, j, k]
+        for i in range(x)
+        for j in range(y)
+        for k in range(z)
+        if i + j + k != n
+    ]
+
+    print(coordinates)
