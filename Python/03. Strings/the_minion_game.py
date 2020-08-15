861c76bd9248937898000d114c40cf83cd2ce991
João Palmeiro <joaommpalmeiro@gmail.com>
2020-08-16 00:17:08 +0100

    Updated: the_minion_game.

--- Python/03. Strings/the_minion_game.py
+++ Python/03. Strings/the_minion_game.py
@@ -1,7 +1,11 @@
+from collections import Counter
 from itertools import islice, tee
+from typing import Iterable, Iterator
+
+VOWELS = "AEIOU"
 
 
-def nwise(iterable, n=2):
+def nwise(iterable: Iterable, n: int = 2) -> Iterator:
     iters = tee(
         iterable, n
     )  # `tee` returns `n` independent iterators from a single iterable
@@ -12,13 +16,51 @@ def nwise(iterable, n=2):
     return zip(*iters)  # The `zip` function ensures groups of size `n` only
 
 
-# Vowels are only defined as AEIOU.
-# Here, Y is not considered a vowel.
-def minion_game(string):
+# It does not run within the time limit.
+# It is not optimized large strings at all.
+def minion_game_first_try(string: str) -> None:
+    vowels_score = 1 if string.startswith(tuple(VOWELS)) else 0
+    consonants_score = 1 if not string.startswith(tuple(VOWELS)) else 0
+
     for length in range(1, len(string)):
-        print(list(nwise(string, length)))
+        cnt = Counter(nwise(string, length))
+        vowels_cnt = sum(v for k, v in cnt.items() if k[0] in VOWELS)
+
+        vowels_score += vowels_cnt
+        consonants_score += sum(cnt.values()) - vowels_cnt
+
+    if vowels_score == consonants_score:
+        print("Draw")
+    elif vowels_score > consonants_score:
+        print(f"Kevin {vowels_score}")
+    else:
+        print(f"Stuart {consonants_score}")
+
+
+def minion_game(string: str) -> None:
+    vowels_score = 0
+    consonants_score = 0
+
+    length = len(string)
+
+    # Each substring at position `i` starts with the same character.
+    # With this in mind, it is enough to iterate over the string once.
+    for i, l in enumerate(string):
+        if l in VOWELS:
+            vowels_score += length - i
+        else:
+            consonants_score += length - i
+
+    if vowels_score == consonants_score:
+        print("Draw")
+    elif vowels_score > consonants_score:
+        print(f"Kevin {vowels_score}")
+    else:
+        print(f"Stuart {consonants_score}")
 
 
 if __name__ == "__main__":
     s = input()
+
+    # minion_game_first_try(s)
     minion_game(s)
